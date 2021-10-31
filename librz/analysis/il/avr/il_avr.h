6a27aaa7094679a67a02571da9ab11c3c0f919a0
wargio <wargio@libero.it>
2021-10-31 13:49:58 +0100

    SPDX header

--- librz/analysis/il/avr/il_avr.h
+++ librz/analysis/il/avr/il_avr.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZIL_ANALYSIS_AVR_H
 #define RZIL_ANALYSIS_AVR_H
 
